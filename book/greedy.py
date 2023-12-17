7e62151a05dc2a3c71e5c3fd185f61deac1c9d6f
hyeilly <dev.jiine@gmail.com>
2023-12-17 17:24:33 +0900

    solved ex2

--- book/greedy.py
+++ book/greedy.py
@@ -19,18 +19,33 @@ class Greedy:
             coin_cnt = coin_r // coin
             cnt += coin_cnt
             coin_r = coin_r - (coin_cnt * coin)
-        return cnt
+        return f"결과 : {cnt}"
 
     def ex2(self):
         """
         큰 수 법칙
+        다양한 수로 이루어지니 배열 => 주어진 수들을 M번 더해 가장 큰 수 만들기
+        특정 인덱스에 해당하는 수가 연속해서 K번 초과해 더해질 수 없는 것이 특징
         첫째줄 N(2<=N<=1000), K(1<=K<=10000)
         둘째줄 N개 자연수
         입력예시)
         5 8 3
         2 4 5 4 6
         """
-        pass
+        i, m, k = input().split()
+        i, m, k = int(i), int(m), int(k)
+        input_list = input().split()
+        input_list = [int(a) for a in input_list]
+        result = 0
+        max_fir = max(input_list)
+        input_list.remove(max_fir)
+        max_sec = max(input_list)
+        for i in range(1, m+1):
+            if i % k == 0:
+                result += max_sec
+            else:
+                result += max_fir
+        return f"결과 : {result}"
 
     def ex3(self):
         """
@@ -45,4 +60,5 @@ class Greedy:
         pass
 
 # if __name__ == '__main__':
-#     Greedy.ex1('',34230)
+    # Greedy.ex1('',34230)
+    # Greedy.ex2('', )
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+# 시간 측정
+import math
+import time
+
 from book.greedy import Greedy
 
 g = Greedy()
-print("문제 요약", g.ex1.__doc__)
-print("실행", g.ex1(1260))
 
+print("문제 :", g.ex1.__doc__)
+start_time = time.process_time()
+print(g.ex1(18022))
+end_time = time.process_time()
+print(f"실행시간 : {int(round((end_time - start_time) * 1000))}ms")
+
+
+print("문제 :", g.ex2.__doc__)
+start_time = time.process_time()
+print(g.ex2())
+end_time = time.process_time()
+print(f"실행시간 : {int(round((end_time - start_time) * 1000))}ms")
